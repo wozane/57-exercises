86dbde53cf8b15e2887fbf1f6b0b85a45ef2c9bc
Aneta Wozniak <wozniakaneta5@gmail.com>
2018-05-28 15:15:43 +0200

    Add simple password check

---
+++ making_decisions/password_validation.test.rb
@@ -0,0 +1,15 @@
+gem 'minitest', '~> 5.0'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'minitest/benchmark'
+require_relative 'password_validation'
+
+class TestPasswordValidation < MiniTest::Test
+  def test_incorrect_password
+    assert_equal false, check_password('abc')
+  end
+
+  def test_correct_password
+    assert_equal true, check_password('abc$123')
+  end
+end
