dec2fff6eb28152de8c1971b5262e2b4dd7995c2
Aneta Wozniak <wozniakaneta5@gmail.com>
2018-07-09 11:09:23 +0200

    Add test case

--- working_with_files/word_frequency_test.rb
+++ working_with_files/word_frequency_test.rb
@@ -7,8 +7,8 @@ class WordFrequencyTest < MiniTest::Test
     @csv = []
     file = File.join(File.expand_path(File.dirname(__FILE__)), 'word.txt')
 
-    File.open(file, 'r') do |file|
-      file.each_line do |line|
+    File.open(file, 'r') do |text|
+      text.each_line do |line|
         arr = line.split(' ')
         arr.each do |word|
           @csv << word
@@ -19,5 +19,11 @@ class WordFrequencyTest < MiniTest::Test
 
   def test_sanity
     assert true, 'this should be true'
+  end
+
+  def test_print_names_and_stars
+    result = { 'badger' => 7, 'mushroom' => 2, 'snake' => 1}
+
+    assert_equal result, print_names_and_stars(@csv)
   end
 end
