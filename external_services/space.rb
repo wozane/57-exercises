94985705faec58a28ab7aab3075066a21764e360
Aneta Wozniak <wozniakaneta5@gmail.com>
2018-07-09 14:07:35 +0200

    Create table with people in space

---
+++ external_services/space.rb
@@ -0,0 +1,16 @@
+require 'httparty'
+require 'json'
+require 'terminal-table'
+
+response = HTTParty.get('http://api.open-notify.org/astros.json').to_s
+my_response = JSON.parse(response)
+people_in_space = my_response['people']
+
+rows = []
+people_in_space.each do |post|
+  rows << [post['name'], post['craft']]
+end
+
+table = Terminal::Table.new :headings => ['Name', 'Craft'], :rows => rows
+
+puts table
