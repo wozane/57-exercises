2cc1e331393c51fcb1f94a204747bd2178504158
Aneta Wozniak <wozniakaneta5@gmail.com>
2018-08-23 22:41:08 +0200

    Wip

--- working_with_files/word_v2.rb
+++ working_with_files/word_v2.rb
@@ -1,9 +1,46 @@
+FILE = File.join(File.expand_path(File.dirname(__FILE__)), 'word.txt')
 class FileReader
-  def initialize(file)
+  def initialize(file = FILE)
     @file = file
   end
 
   def call
-    
+    File.open(file).each do |line|
+      return line
+    end
+  end
+
+  private
+
+  attr_reader :file
+end
+
+# p FileReader.new.call
+
+class ChangeStringToArray
+  def change_to_array(string)
+    text = []
+    text << string
+    text.to_a
+    text
   end
 end
+
+# foo = FileReader.new.call
+# bar = ElementExtraction.new.change_to_array(foo)
+
+# puts bar.class # => array
+
+class ExtractKeysAndValues
+  
+end
+
+class Calculator
+  def add(*arg)
+    arg.reduce(:+)
+  end
+end
+
+# cal = Calculator.new
+# p cal.add(1,2)
+
---
+++ working_with_files/word_v2_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'word_v2'
+
+class TestWord < MiniTest::Test
+  
+end
