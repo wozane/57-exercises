f56e3a60a1f6081c5e15348bc0bb10fea68d53aa
Aneta Wozniak <wozniakaneta5@gmail.com>
2018-08-27 11:35:01 +0200

    WIP mainProgram call

--- working_with_files/word_v2.rb
+++ working_with_files/word_v2.rb
@@ -1,4 +1,5 @@
 FILE = File.join(File.expand_path(File.dirname(__FILE__)), 'word.txt')
+
 class FileReader
   def initialize(file = FILE)
     @file = file
@@ -15,32 +16,76 @@ class FileReader
   attr_reader :file
 end
 
-# p FileReader.new.call
-
 class ChangeStringToArray
-  def change_to_array(string)
+  def initialize(string)
+    @string = string
+  end
+
+  def call
     text = []
     text << string
     text.to_a
     text
   end
+
+  private
+  attr_reader :string
+end
+
+class ExtractKeysAndValues
+  def initialize(unsorted_array)
+    @unsorted_array = unsorted_array
+  end
+
+  def call
+    sorted_array = unsorted_array.group_by(&:itself).transform_values(&:count)
+    sorted_array
+  end
+
+  private
+  attr_reader :unsorted_array
+end
+
+class PrintData < ExtractKeysAndValues
+  def initialize(sorted_array = ExtractKeysAndValues)
+    @sorted_array = sorted_array
+  end
+
+  def call
+    sorted_array.each do |key, value|
+      "#{key}: #{'*' * value}"
+    end
+  end
+
+  private
+  attr_reader :sorted_array
+end
+
+class MainProgram
+  def initialize(file = FileReader, changer = ChangeStringToArray, extractor = ExtractKeysAndValues, presenter = PrintData)
+    @file = file
+    @changer = changer
+    @extractor = extractor
+    @presenter = presenter
+  end
+
+  def call
+    raw_array = file.new.call
+    changed_array = changer.new(raw_array).call
+    extracted_array = extractor.new(changed_array).call
+    prepared_array = presenter.new(extracted_array).call
+    prepared_array
+  end
+
+  private
+
+  attr_reader :file, :changer, :extractor, :presenter
+end
+
+if __FILE__ == $0
+  puts MainProgram.new.call
 end
 
 # foo = FileReader.new.call
-# bar = ElementExtraction.new.change_to_array(foo)
 
-# puts bar.class # => array
-
-class ExtractKeysAndValues
-  
-end
-
-class Calculator
-  def add(*arg)
-    arg.reduce(:+)
-  end
-end
-
-# cal = Calculator.new
-# p cal.add(1,2)
-
+# p foo.split(' ').group_by(&:itself).transform_values(&:count)
--- working_with_files/word_v2_test.rb
+++ working_with_files/word_v2_test.rb
@@ -3,5 +3,4 @@ require 'minitest/pride'
 require_relative 'word_v2'
 
 class TestWord < MiniTest::Test
-  
 end
